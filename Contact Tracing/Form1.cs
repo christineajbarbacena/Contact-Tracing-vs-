e0f3933f6ce8db8de6dd86095015d5dc1695be10
christineajbarbacena <christinebarbacena9@gmail.com>
2022-06-09 14:40:46 +0800

    file.Writeline added up to Contact Info

--- Contact Tracing/Form1.cs
+++ Contact Tracing/Form1.cs
@@ -36,7 +36,27 @@ namespace Contact_Tracing
         private void bttnSubmit_Click(object sender, EventArgs e)
         {
             StreamWriter file = new StreamWriter(@"C:\Users\barbacena\Documents\VisualStudioCTA\CTresponse.txt", true);
-            file.WriteLine();
+            file.WriteLine("Response of Client " + txtboxLN.Text);
+            file.WriteLine("First Name: " + txtboxFN.Text);
+            file.WriteLine("Middle Name: " + txtboxMN.Text);
+            file.WriteLine("Last Name: " + txtboxLN.Text);
+            file.WriteLine("Suffix: " + txtboxSuffix.Text);
+            file.WriteLine("Birth Date: " + txtboxbirthdate.Text);
+            file.WriteLine("Age: " + txtboxage.Text);
+            file.WriteLine("Gender: " +txtboxgender.Text);
+            file.WriteLine("Civil Status: " + txtboxcivilstatus.Text);
+            file.WriteLine("Region:" + txtboxRegion.Text);
+            file.WriteLine("City: " + txtboxCity.Text);
+            file.WriteLine ("Street Name: " + txtboxStreet.Text);
+            file.WriteLine ("Barangay: " + txtboxBarangay.Text);
+            file.WriteLine ("Email Address:" + txtboxEmailAdd.Text);
+            file.WriteLine ("Mobile Number:" + txtboxMobileNum.Text);
+            file.WriteLine ("Telephone Number:" + txtboxTelephonNum.Text);
+            file.WriteLine ("" + );
+            file.WriteLine ("" + );
+            file.WriteLine ("" + );
+            file.WriteLine ("" + );
+            file.WriteLine ("" + );
             file.Close();
         }
     }
